e7f0e8427118723782a4639e0b09dde41b4fd877
MGAronya <20jzhang@stu.edu.cn>
2022-10-10 00:13:40 +0800

    多路复合计算器

---
+++ multi-channel-recombination.go
@@ -0,0 +1,71 @@
+// @Title  multi-channel-recombination
+// @Description  多路复合计算器
+// @Author  MGAronya（张健）
+// @Update  MGAronya（张健）  2022-10-09 23:54
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// @title    doCompute
+// @description   这个函数可以用来处理比较耗时的事情，比如计算
+// @auth      MGAronya（张健）             2022-10-09 23:54
+// @param     x int				入参
+// @return    int				结果
+func doCompute(x int) int {
+	// TODO 模拟计算
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+
+	// TODO 此处假设1 + x是一个很费时的计算
+	return 1 + x
+}
+
+// @title    branch
+// @description   每个分支开出一个goroutine来做计算，并把计算结果发送到各自通道里
+// @auth      MGAronya（张健）             2022-10-09 23:54
+// @param     x int					入参
+// @return    chan int				用于传出结果
+func branch(x int) chan int {
+	ch := make(chan int)
+	// TODO 开出一个goroutine来做计算，并把计算结果发送通道里
+	go func() {
+		ch <- doCompute(x)
+	}()
+	return ch
+}
+
+// @title    Recombination
+// @description   将传入的多路通道复合
+// @auth      MGAronya（张健）             2022-10-09 23:54
+// @param     chs ...chan int			多路通道
+// @return    chan int					用于传出结果的通达
+func Recombination(chs ...chan int) chan int {
+	ch := make(chan int)
+
+	for _, c := range chs {
+		// TODO 此处要明确传值
+		go func(c chan int) {
+			// TODO 复合
+			ch <- <-c
+		}(c)
+	}
+
+	return ch
+}
+
+// @title    main
+// @description   进行多路复合计算
+// @auth      MGAronya（张健）             2022-10-09 23:54
+// @param     void
+// @return    void
+func main() {
+	// TODO 返回多路复合的结果
+	result := Recombination(branch(10), branch(20), branch(30))
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(<-result)
+	}
+}
